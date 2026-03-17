c19d4e357020ac9f407e8caf0cffe8edc8d89874
Marcos <marcosagieco@gmail.com>
2026-03-16 21:49:12 -0300

    actualizacion 72 /sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,25 +1,10 @@
 export default function sitemap() {
-  const baseUrl = 'https://028import.com';
-
   return [
     {
-      url: baseUrl,
+      url: 'https://028import.com',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
-    },
-    // Agregamos las vistas legales como si fueran páginas para que Google las valore
-    {
-      url: `${baseUrl}/nosotros`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
-    },
-    {
-      url: `${baseUrl}/envios`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
     },
   ];
 }
